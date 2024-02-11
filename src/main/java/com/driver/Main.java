711ec1b6f25d4bcd7f194c72ad1ecad3e7085642
Rameshwar Tale <ramgtale21@gmail.com>
2024-02-11 11:17:56 +0530

    add all file

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+
+        RWOnly obj = new RWOnly();
+
+        //obj.name = "Rameshwar";
+
+      //   java: name has private access in com.driver.RWOnly
+
+        obj.setName("Rameshwar");
+
+        System.out.println(obj.getName());
+    }
 }
